7a49cecd833fadd294ef40643fbb13be4adab67f
한승준 <tmdwns950428@naver.com>
2023-06-12 16:04:52 +0900

    accessToken problem

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,23 +1,21 @@
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import styles from "../styles/home.module.css";
 
 const Home = () => {
-  const navigate = useNavigate();
-
   return (
     <div className={styles["signin-container"]}>
       {/* 홈 제목 */}
       <h2>Home</h2>
 
       {/* 회원가입 버튼 */}
-      <button className={styles["button"]} onClick={() => navigate("/signup")}>
+      <Link to="/signup" className={styles["button"]}>
         SignUp
-      </button>
+      </Link>
 
       {/* 로그인 버튼 */}
-      <button className={styles["button"]} onClick={() => navigate("/signin")}>
+      <Link to="/signin" className={styles["button"]}>
         SignIn
-      </button>
+      </Link>
     </div>
   );
 };
